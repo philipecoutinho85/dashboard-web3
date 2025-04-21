1a0bed0c00ffce7a2ac23f5fc4a6b68a3e034fb6
philipecoutinho85 <philipecoutinhor@gmail.com>
2025-04-21 19:46:50 -0300

    Update useWallet.js

--- src/hooks/useWallet.js
+++ src/hooks/useWallet.js
@@ -4,34 +4,46 @@ export default function useWallet() {
   const [walletAddress, setWalletAddress] = useState('');
 
   const connectWallet = async () => {
-    if (window.ethereum) {
-      try {
-        const accounts = await window.ethereum.request({
-          method: 'eth_requestAccounts'
-        });
-        setWalletAddress(accounts[0]);
-      } catch (err) {
-        console.error('Erro ao conectar carteira:', err);
-      }
-    } else {
+    if (!window.ethereum) {
       alert('MetaMask não encontrada');
+      return;
+    }
+
+    try {
+      const accounts = await window.ethereum.request({
+        method: 'eth_requestAccounts'
+      });
+      if (accounts.length > 0) {
+        setWalletAddress(accounts[0]);
+      }
+    } catch (err) {
+      console.error('Erro ao conectar carteira:', err);
     }
   };
 
   useEffect(() => {
     const checkConnection = async () => {
       if (window.ethereum) {
-        const accounts = await window.ethereum.request({ method: 'eth_accounts' });
-        if (accounts.length > 0) {
-          setWalletAddress(accounts[0]);
+        try {
+          const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+          if (accounts.length > 0) {
+            setWalletAddress(accounts[0]);
+          }
+        } catch (err) {
+          console.error('Erro ao verificar contas:', err);
         }
       }
     };
+
     checkConnection();
 
     if (window.ethereum) {
       window.ethereum.on('accountsChanged', (accounts) => {
         setWalletAddress(accounts[0] || '');
+      });
+
+      window.ethereum.on('disconnect', () => {
+        setWalletAddress('');
       });
     }
   }, []);
