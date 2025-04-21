6be132a7831f876b09ce654cd4bc1df81456f9a2
philipecoutinho85 <philipecoutinhor@gmail.com>
2025-04-21 19:02:01 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -14,4 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app); // <-- Novo: conexão Firestore
+export const db = getFirestore(app); // ✅ Conexão Firestore ativada
