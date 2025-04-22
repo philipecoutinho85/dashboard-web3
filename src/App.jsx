7a6d60de7aab19a3d6944da5d620de7ba9cd06c4
philipecoutinho85 <philipecoutinhor@gmail.com>
2025-04-22 19:29:44 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Admin from './pages/Admin';
 
-import ExplorerDocumentos from './pages/Explorer';
+import Explorer from './pages/Explorer'; // ✅ corrigido aqui
 import ValidarDocumento from './pages/ValidarDocumento';
 
 export default function App() {
