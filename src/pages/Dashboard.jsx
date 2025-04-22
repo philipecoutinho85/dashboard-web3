6b982199ba9fc999a1ff881b4c248b4051d52655
philipecoutinho85 <philipecoutinhor@gmail.com>
2025-04-22 13:04:38 -0300

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from '@/components/Header';
-import useWallet from '@/hooks/useWallet';
+// import Header from '@/components/Header';
+// import useWallet from '@/hooks/useWallet';
 
 export default function Dashboard() {
   const { walletAddress, connectWallet } = useWallet();
