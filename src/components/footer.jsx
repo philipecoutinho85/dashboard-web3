6f84f95bcf644b6a695a9716859adf9736cec747
philipecoutinho85 <philipecoutinhor@gmail.com>
2025-04-23 01:29:13 -0300

    Create footer.jsx

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="text-center py-4 mt-10 text-xs text-gray-500 dark:text-gray-400">
+      MVP desenvolvido por <strong>Philipe Coutinho</strong> — <a href="https://p.coutinho.com.br" className="text-[#ff385c] hover:underline">p.coutinho.com.br</a>
+    </footer>
+  );
+};
+
+export default Footer;
